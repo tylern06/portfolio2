87261229a94c263a2ce68e1b74d8c3ccdbe1d0ed
Tyler Nguyen <tynguyen06@gmail.com>
2017-01-05 17:33:40 -0800

    updated css

--- client/js/custom.js
+++ client/js/custom.js
@@ -54,45 +54,6 @@
 			$(".js-height-full").height($(window).height());
 		});
 
-		/* ---------------------------------------------- /*
-		 * Twitter
-		/* ---------------------------------------------- */
-
-		var twitterConfig = {
-			id: '345170787868762112',
-			domId: '',
-			maxTweets: 3,
-			enableLinks: true,
-			showUser: false,
-			customCallback: handleTweets
-		};
-
-		twitterFetcher.fetch(twitterConfig);
-
-		function handleTweets(tweets) {
-			var x = tweets.length;
-			var n = 0;
-			var html = '';
-			while(n < x) {
-				html += '<div class="owl-item">' + tweets[n] + '</div>';
-				n++;
-			}
-			$('.testimonials').html(html);
-
-			$('.twitter_retweet_icon').html('<i class="fa fa-retweet"></i>');
-			$('.twitter_reply_icon').html('<i class="fa fa-reply"></i>');
-			$('.twitter_fav_icon').html('<i class="fa fa-star"></i>');
-
-			$('.testimonials').owlCarousel({
-				singleItem: true,
-				navigation: false,
-				pagination: false,
-				slideSpeed : 300,
-				paginationSpeed : 400,
-				autoPlay: 5000,
-				navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
-			});
-		}
 
 		/* ---------------------------------------------- /*
 		 * Initialize shuffle plugin
@@ -112,15 +73,15 @@
 			$portfolioContainer.shuffle('shuffle', groupName );
 		});
 
-		$('.simple-ajax-popup').magnificPopup({
-			type: 'ajax',
-			callbacks: {
-				parseAjax: function(mfpResponse) {
-					$.getScript('assets/js/jquery.fitvids.js');
-					$.getScript('assets/js/custom-portfolio.js');
-				},
-			}
-		});
+		// $('.simple-ajax-popup').magnificPopup({
+		// 	type: 'ajax',
+		// 	callbacks: {
+		// 		parseAjax: function(mfpResponse) {
+		// 			$.getScript('assets/js/jquery.fitvids.js');
+		// 			$.getScript('assets/js/custom-portfolio.js');
+		// 		},
+		// 	}
+		// });
 
 		/* ---------------------------------------------- /*
 		 * WOW Animation When You Scroll
@@ -135,7 +96,6 @@
 		 * A jQuery plugin for fluid width video embeds
 		/* ---------------------------------------------- */
 
-		$('body').fitVids();
 
 		/* ---------------------------------------------- /*
 		 * E-mail validation
@@ -195,36 +155,6 @@
 
 		});
 
-		/* ---------------------------------------------- /*
-		 * Google Map
-		/* ---------------------------------------------- */
-
-		var mapLocation = new google.maps.LatLng(34.031428,-118.2071542,17);
-
-		map = new GMaps({
-			streetViewControl : false,
-			overviewMapControl: false,
-			mapTypeControl: false,
-			zoomControl : false,
-			panControl : false,
-			scrollwheel: false,
-			center: mapLocation,
-			el: '#map',
-			zoom: 16,
-			styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
-		});
-
-		var image = new google.maps.MarkerImage('assets/images/map-icon.png',
-			new google.maps.Size(80, 80),
-			new google.maps.Point(0, 0),
-			new google.maps.Point(40, 40)
-		);
-
-		map.addMarker({
-			position: mapLocation,
-			icon: image,
-			animation: google.maps.Animation.BOUNCE,
-		});
 
 	});
 
